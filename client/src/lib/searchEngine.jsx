eb49dcbe1319a33f37b4b10aa27c90371f3ee8f2
William Chang <wdchang86@gmail.com>
2021-01-14 17:23:59 -0500

    Created review GET Requests

--- client/src/lib/searchEngine.jsx
+++ client/src/lib/searchEngine.jsx
@@ -2,36 +2,37 @@ import React from "react";
 import axios from "axios";
 
 let searchEngine = {
-  get : ()=>{
-    // GET /products
-    //   query: page, count
-    // GET /products/:product_id
-    // GET /products/:product_id/styles
-    // GET /cart, POST /cart
-    // GET /reviews
-    //   query: page, count, sort,
-    // GET /reviews/meta/?product_id
-    // GET /qa/questions
-    //   query: product_id, page, count
-    //  GET /qa/questions/:question_id/answers
-    //   query: page, count
-    //  GET /products/:product_id/related
+
+  getReviews: (productId, page, count, sort) => {
+    axios({
+      method: 'GET',
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/reviews',
+      params: {product_id: productId, page, count, sort}
+    })
+      .then(result => {
+        return result.data;
+      })
+      .catch(err => console.error(err));
   },
-  post : ()=>{
-    // POST /reviews
-    //   body: product_id + 8 parameters
-    // POST /qa/questions
-    //   body: product_id + 3 parameters
-    // POST /qa/questions/:question_id/answers
+
+  getProductReviewData: (productId) => {
+    axios({
+      method: 'GET',
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/reviews/meta',
+      params: {product_id: productId}
+    })
+      .then(result => {
+        return result.data;
+      })
+      .catch(err => console.error(err));
   },
-  put :() =>{
-    // PUT /reviews/:review_id/helpful
-    // PUT /reviews/:review_id/report
-    // PUT /qa/answers/:answer_id/helpful
-    // PUT /qa/answers/:answer_id/report
-    // PUT /qa/questions/:question_id/helpful
-    // PUT /qa/questions/:question_id/report
+
+  post: () => {
+  },
+
+  put: () =>{
+
   }
-}
+};
 
 export default searchEngine;
