5064bb4e847348eca91b1de8b59baa9c569345fb
claire <kimsujin8702@gmail.com>
2021-01-14 01:57:34 -0500

    import react-redux to use redux

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store/store.jsx';
+// import store from './store/store.jsx';
 import App from "./components/App.jsx";
 
 ReactDOM.render(
