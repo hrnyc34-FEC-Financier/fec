5c496165b8cb45d8a5ebaa48cf69d69ceaff7adc
claire <kimsujin8702@gmail.com>
2021-01-15 23:41:06 -0500

    Initial master branch

--- client/src/actions/relatedItemsSearch.jsx
+++ client/src/actions/relatedItemsSearch.jsx
@@ -3,29 +3,25 @@ import { API_TOKEN } from '../../../env/config.js';
 import relatedItems from './relatedItems.jsx';
 
 let relatedItemsSearch = ( productId ) => {
-  console.log('relatedItemsSearched_id:', productId);
 
   return function( dispatch ) {
     let options = {
-      url: `/products/${productId}/related`,
-      headers: { 'Authorization': `token ${ API_TOKEN }`},
-      params: { product_id: productId }
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${productId}/related`,
+      headers: { 'Authorization': API_TOKEN },
+      // params: { product_id: productId }
     };
 
-    searchEngine.get( options, ( relatedItemList ) => {
-      dispatch( relatedItems( relatedItemList ) );
-    });
+    searchEngine.get( options )
+      .then( relatedItemList => {
+        dispatch( relatedItems( relatedItemList ) );
+      });
 
-    // post( options, ( results ) => {
+    // searchEngine.post( options, ( results ) => {
     //   dispatch( addButton( results ) );
     // });
 
-    // put( options, ( results ) => {
+    // searchEngine.put( options, ( results ) => {
     //   dispatch( addButton( results ) );
     // });
-
   };
-
 };
-
-export default relatedItemsSearch;
