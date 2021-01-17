cfb58465bcbad5aa17067224a4d1c111dd97a8ab
akolatch <aaron.kolatch@gmail.com>
2021-01-17 16:42:18 -0500

    implement style selction functionality

--- client/src/components/ProductDetail/StyleSelector.jsx
+++ client/src/components/ProductDetail/StyleSelector.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
-const StyleSelector = ({ currentStyleList, currentStyle }) => (
+const StyleSelector = ({ currentStyleList, currentStyle, styleChange }) => (
   <div className='style-selector'>
     <div className='style-name'>{currentStyle.name}</div>
     {currentStyleList.map((style, i) => (
-      <div
+      <button
+        onClick={styleChange}
         className='thumbnail thumb-round'
         key={style.style_id}
         width='50px'
         height='50px'
+        value={JSON.stringify(style)}
         style={{
           backgroundImage: `url(${style.photos[0].thumbnail_url})`,
         }}
-      ></div>
+      ></button>
     ))}
   </div>
 );
