bbc1c477d5823d06e7abfdfba8445e39980081f4
William Chang <wdchang86@gmail.com>
2021-01-18 07:52:41 -0500

    Added react-icons. Added starRatingReadOnly component. Added reviewCharacteristics action/redicer.

--- client/src/actions/RatingsReviews/setRatings.js
+++ client/src/actions/RatingsReviews/setRatings.js
@@ -2,6 +2,18 @@ import searchAPI from '../../lib/searchEngine.js';
 import changeProductCharacteristics from './productRatings/productCharacteristics.js';
 import changeProductRatings from './productRatings/productRatings.js';
 import changeProductRecommended from './productRatings/productRecommended.js';
+import changeProductAvgRating from './productRatings/productAvgRating.js';
+
+const calculateProductAvgRating = (productRatings) => {
+  let totalRating = 0;
+  let totalReviews = 0;
+  for (let star in productRatings) {
+    const numberReviews = Number(productRatings[star]);
+    totalRating += Number(star) * numberReviews;
+    totalReviews += numberReviews;
+  }
+  return Number((totalRating / totalReviews).toFixed(1));
+};
 
 const setRatings = (product_id) => {
   return (dispatch) => {
@@ -10,6 +22,7 @@ const setRatings = (product_id) => {
         dispatch(changeProductRatings(data.ratings));
         dispatch(changeProductRecommended(data.recommended));
         dispatch(changeProductCharacteristics(data.characteristics));
+        dispatch(changeProductAvgRating(calculateProductAvgRating(data.ratings)));
       })
       .catch((err) => console.error('Unable to get Product Rating Data:', err));
   };
