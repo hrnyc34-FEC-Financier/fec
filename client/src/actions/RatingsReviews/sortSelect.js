684c56c2f7f40508d09e84ac9083f580ab38cefc
William Chang <wdchang86@gmail.com>
2021-01-19 14:42:13 -0500

    Added sort functionality

--- client/src/actions/RatingsReviews/sortSelect.js
+++ client/src/actions/RatingsReviews/sortSelect.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import searchAPI from '../../lib/searchEngine.js';
 import changeReviewSort from './reviewsList/reviewSort.js';
+import changeReviewsList from './reviewsList/reviewsList.js';
 
 const sortSelect = (product_id, sort) => {
   // console.log(sort);
   return (dispatch) => {
     return searchAPI.get('reviews', {product_id, sort})
-      .then(() => {
+      .then(({ data }) => {
         dispatch(changeReviewSort(sort));
+        dispatch(changeReviewsList(data.results));
       })
       .catch(err => console.error('Unable to process sortBy Request', err));
   };
