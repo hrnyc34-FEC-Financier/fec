027a3f8658a16b7284015813f8fae295dcafe687
akolatch <aaron.kolatch@gmail.com>
2021-01-20 10:35:26 -0500

    styling add

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,16 +4,14 @@ import RatingsReviewsMain from './RatingsReviews/RatingsReviewsMain.jsx';
 import RelatedItemsContainer from '../containers/RelatedItems/RelatedItems.js';
 import QAModuleContainer from '../containers/QAContainers/QAModuleContainer.js';
 
-
 const App = ({ teamName }) => {
   return (
     <div>
-    {/* <div className='container'> */}
       <ProductDetailContainer />
       <RelatedItemsContainer />
       <QAModuleContainer />
       <div className='reviewsWidgetContainer'>
-        < RatingsReviewsMain />
+        <RatingsReviewsMain />
       </div>
     </div>
   );
