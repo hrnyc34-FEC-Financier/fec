e0b25d30091a6d125842dec8ea0ee3c62cabdadf
akolatch <aaron.kolatch@gmail.com>
2021-01-21 09:46:36 -0500

    minor css

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import RatingsReviewsMain from './RatingsReviews/RatingsReviewsMain.jsx';
 import RelatedItemsContainer from '../containers/RelatedItems/RelatedItems.js';
 import QAModuleContainer from '../containers/QAContainers/QAModuleContainer.js';
-import './RatingsReviews/will-style.css';
+// import './RatingsReviews/will-style.css';
 import ProductDetailMain from '../components/ProductDetail/ProductDetailMain.jsx';
 const App = () => {
   return (
