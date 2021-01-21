b5c04edfb7d69d5e5d9f2b6242658bc2d335e7e1
akolatch <aaron.kolatch@gmail.com>
2021-01-21 14:56:52 -0500

    remove console log

--- client/src/components/ProductDetail/AddToCart.jsx
+++ client/src/components/ProductDetail/AddToCart.jsx
@@ -11,7 +11,6 @@ const AddToCart = ({
 }) => {
   const favorite = (e) => {
     e.preventDefault();
-    console.log(currentProduct.id);
     if (!outfitList.includes(currentProduct.id)) {
       addToOutfitList(currentProduct.id);
     }
