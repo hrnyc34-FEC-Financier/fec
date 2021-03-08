0c9ab22d13dab2049c53e887a7f68cfddd106056
claire <kimsujin8702@gmail.com>
2021-03-08 03:40:57 -0500

    set up core state for product overview

--- client/src/actions/productDetail/InitialProductList.js
+++ client/src/actions/productDetail/InitialProductList.js
@@ -1,14 +1,12 @@
 import searchAPI from '../../lib/searchEngine.js';
 import setSelectedProduct from './selectProduct.js';
 import setProductList from '../../actions/productDetail/productList.js';
-import addRelatedItems from '../relatedItems/addRelatedItems.js';
 
 const setInitialProductList = () => {
   return (dispatch) => {
     return searchAPI.get('products').then(({ data }) => {
       dispatch(setProductList(data));
       dispatch(setSelectedProduct(data[0].id));
-      dispatch( addRelatedItems( data[0].id ) );
     });
   };
 };
