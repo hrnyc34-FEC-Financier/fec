053be8210f9232fbf8fd8c85fa34e7d23c7bc96e
claire <kimsujin8702@gmail.com>
2021-07-10 03:10:22 -0400

    Fix bug_ youroutfit carousel arrow for index to render items

--- client/src/reducers/RelatedItems/cYourOutfitRenderIndex.js
+++ client/src/reducers/RelatedItems/cYourOutfitRenderIndex.js
@@ -1,23 +1,23 @@
 import Redux from 'redux';
 
 const carouselRenderIndexReducer = (state = 0, action) => {
-  switch ( action.type ) {
+  switch (action.type) {
+    case 'PLUS_INDEX_FOR_RENDER':
+      let increment = action.index && state - 1;
+      if (increment < 0) {
+        return 0;
+      }
+      return increment;
 
-  case 'INCREASE_INDEX_FOR_RENDER':
-    let increment = action.index && state - 1;
-    if (increment < 0) {
-      return 0;
-    }
-    return increment;
+    case 'MINUS_INDEX_FOR_RENDER':
+      let decrement = action.index && state + 1;
+      return decrement;
 
-  case 'DECREASE_INDEX_FOR_RENDER':
-    let decrement = action.index && state + 1;
-    return decrement;
+    case 'RESET_CAROUSEL':
+      return action.reset;
 
-  case 'RESET_CAROUSEL':
-    return action.reset;
-
-  default : return state;
+    default:
+      return state;
   }
 };
 
